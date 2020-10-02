70fbb7f2c4424b24140588e72bf85d42a2620055
Vince Au <vinceau09@gmail.com>
2020-10-02 18:01:13 +1000

    Hello!

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -30,7 +30,7 @@ export const App: React.FC = () => {
   });
   return (
     <div>
-      <h1>Hi there</h1>
+      <h1>Hello!!</h1>
       <div id="notification" className={showNotif ? "" : "hidden"}>
         <p id="message">{notif}</p>
         <button id="close-button" onClick={closeNotification}>
